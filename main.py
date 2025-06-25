5823d8adb8ac5b42b686362ca6250de974b16cb6
Oleksii Haiduk <Silvestr171@gmail.com>
2025-06-25 12:58:57 +0300

    loging

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import urllib.parse
 import mimetypes
 import pathlib
 from datetime import datetime
+import logging
 
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
+        logging.info("do_GET, pr_url: %s", pr_url)
         if pr_url.path == '/':
             self.send_html_file('./front-init/index.html')
         elif pr_url.path == '/message.html':
@@ -30,8 +32,10 @@ class HttpHandler(BaseHTTPRequestHandler):
         mt = mimetypes.guess_type(self.path)
         if mt:
             self.send_header("Content-type", mt[0])
+            logging.info("Content-type %s", mt[0])
         else:
             self.send_header("Content-type", 'text/plain')
+            logging.info("Content-type ", 'text/plain')
         self.end_headers()
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
@@ -42,20 +46,23 @@ class HttpHandler(BaseHTTPRequestHandler):
         data_parse = urllib.parse.unquote_plus(data.decode())
         data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
         message_dict ={message_time: data_dict}
-        print (message_dict)
+        logging.info("Message: %s", message_dict)
         self.send_response(302)
         self.send_header('Location', '/')
         self.end_headers()
 
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
+    logging.info("Start Server")
     server_address = ('localhost', 3000)
     http = server_class(server_address, handler_class)
     try:
         http.serve_forever()
     except KeyboardInterrupt:
         http.server_close()
+        logging.info("Server close")
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
     run()
